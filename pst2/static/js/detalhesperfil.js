eb6ea728c3036fc965f701b21d433659d407161d
Vivianefsm <vivisolon@gmail.com>
2024-02-08 14:20:38 -0300

    Update detalhesperfil.js

--- pst2/static/js/detalhesperfil.js
+++ pst2/static/js/detalhesperfil.js
@@ -1,4 +1,10 @@
 //Habilitar os campos de edição
+/**
+ * A função `habilitarInput()` habilita todos os campos de entrada de um formulário e 
+ * desabilita o botão "Alterar" ao ativar o botão "Salvar".
+ * @date 2024-02-07 
+ * @author Ana Carolina
+ */
 function habilitarInput() {
     const inputs = document.querySelectorAll(".form-control, .form-select");
     inputs.forEach(input => {
