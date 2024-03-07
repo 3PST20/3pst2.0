c2cb61ea6a449f302c1ef9876ce62dc13dbf5ba6
Vivianefsm <vivisolon@gmail.com>
2024-03-07 15:06:14 -0300

    Create conf.py

---
+++ Doc_3PST/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = '3PST_2.0.0'
+copyright = '2024, INPE/COGPI/SEPEC'
+author = 'INPE/COGPI/SEPEC'
+release = '2.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx_js']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'pt_BR'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# -- Diretório do códigos em JavaScript 2024 -----------------
+
+js_source_path = 'C:\\Users\\ere_m\\OneDrive\\Documentos\\GitHub\\3pst2.0\\pst2\\static\\js'
+js_source_path = 'C:\\Users\\ere_m\\OneDrive\\Documentos\\GitHub\\3pst2.0\\doc_3pst\\_build\\html\\_sources\\_static\\inpelogo.png'
