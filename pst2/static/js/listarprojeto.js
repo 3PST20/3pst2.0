868f87d911df080248e798dd20e83dd81a2c1c2a
Vivianefsm <vivisolon@gmail.com>
2024-03-07 15:06:48 -0300

    Update listarprojeto.js

--- pst2/static/js/listarprojeto.js
+++ pst2/static/js/listarprojeto.js
@@ -1,19 +1,28 @@
 // ---------------------------------------------------------
 // INPE/COGPI/SEPEC - Serviço de Planejamento e Controle
 // ---------------------------------------------------------
-// JSDoc 4.0.2
-// Módulo: 
-
+// Versão 3PST 2.0.0
+// Módulo I
+//
+// Ferrementa JSDoc 4.0.2
+//---------------- Integrantes do Projeto 3PST -------------
+// Alberto de Paula Silva
+// Ana Carolina das Neves
+// Ane Caroline Maciel de Lima
+// Bárbara Alessandra Gonçalves Pinheiro Yamada
+// Renato Henrique Ferreira Branco
+// Viviane Ferreira da Silva de Macedo
+//
 //---------------- Histórico de Revisão --------------------
 //      Nome                      Data
-// Ana Carolina Neves          00/00/2023   
+// Ana Carolina das Neves          05/02/2024   
 
 
 /**
  * A função de pesquisa filtra uma tabela com base na entrada do usuário.
- * @date 2024-02-07
- * @author Ana Carolina Neves
- * @version 2.0.0 - A versão do projero 3PST.
+ * @date 2023-12-01
+ * @author Ana Carolina das Neves
+ * @version 3PST-2.0.0 - A versão do projero 3PST.
  */
 function search() {
     var input, filter, table, tr, td, i, j, txtValue;
@@ -40,9 +49,9 @@ function search() {
 /**
  * A função "registerProject" solicita ao usuário que confirme se deseja registrar 
  * um novo projeto e os redireciona para a página de registro se confirmarem.
- * @date 2024-02-07
+ * @date 2023-12-01
  * @author Ana Carolina Neves
- * @version 2.0.0 - A versão do projero 3PST.
+ * @version 3PST-2.0.0 - A versão do projero 3PST.
  */
 function registerProject() {
     let text = "Gostaria de cadastrar um novo projeto?";
@@ -54,9 +63,9 @@ function registerProject() {
 
 /**
  * A função `htmlToCsv` converte uma tabela HTML em um arquivo CSV e faz o download.
- * @date 2024-02-07
- * @author Ana Carolina Neves
- * @version 2.0.0 - A versão do projero 3PST.
+ * @date 2023-12-01
+ * @author Ana Carolina das Neves
+ * @version 3PST-2.0.0 - A versão do projero 3PST.
  * @param { string } filename - O parâmetro `filename` é uma string que representa 
  * o nome do arquivo CSV que será baixado.
  */
@@ -78,16 +87,17 @@ function htmlToCsv(filename) {
 
 /**
  * A função `downloadCSVFile` é responsável por criar um arquivo CSV e iniciar seu download.
- * @date 2024-02-07
- * @author Ana Carolina Neves
- * @version 2.0.0 - A versão do projero 3PST.
+ * @date 2023-12-01
+ * @author Ana Carolina das Neves
+ * @version 3PST-2.0.0 - A versão do projero 3PST.
  * @param { string } csv - O parâmetro `csv` é uma string que representa o conteúdo do arquivo CSV que você deseja baixar.
  * @param { string } filename - O parâmetro 'filename' é uma string que especifica o nome do arquivo a ser baixado.
+ * @param { string } tagname - Cria uma instância do elemento para a tag especificada.
  */
 function downloadCSVFile(csv, filename) {
     var csv_file, download_link
     csv_file = new Blob([csv], { type: "text/csv" });
-    download_link = document.createElement("a")
+    download_link = document.createElement("a") 
     download_link.download = filename
     download_link.href = window.URL.createObjectURL(csv_file)
     download_link.style.display = "none"
